05f1825c1294fe83b9a52080ab4cc2dbfe656130
Mathieu David <mathieudavid@mathieudavid.org>
2015-08-06 21:40:38 +0000

    rebuild pages at 3baa916

--- implementors/rustc_serialize/json/trait.ToJson.js
+++ implementors/rustc_serialize/json/trait.ToJson.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustc_serialize'] = [];implementors['mdbook'] = ["impl <a class='trait' href='rustc_serialize/json/trait.ToJson.html' title='rustc_serialize::json::ToJson'>ToJson</a> for <a class='struct' href='mdbook/book/bookitem/struct.BookItem.html' title='mdbook::book::bookitem::BookItem'>BookItem</a>",];
+implementors['rustc_serialize'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
