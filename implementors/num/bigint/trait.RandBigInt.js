711c26b4cd87df283232e07676e79f3a18d05c1e
Mathieu David <mathieudavid@mathieudavid.org>
2015-08-06 21:40:48 +0000

    rebuild pages at 3baa916

--- implementors/num/bigint/trait.RandBigInt.js
+++ implementors/num/bigint/trait.RandBigInt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];
+implementors['num'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
