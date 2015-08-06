5d578533f4bc9ca542c192b940ab0321ab677209
Mathieu David <mathieudavid@mathieudavid.org>
2015-08-06 21:06:59 +0000

    rebuild pages at bf114d7

--- implementors/core/str/pattern/trait.TwoWayStrategy.js
+++ implementors/core/str/pattern/trait.TwoWayStrategy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['num'] = [];
+implementors['regex_syntax'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
