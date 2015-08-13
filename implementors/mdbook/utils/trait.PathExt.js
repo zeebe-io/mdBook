af06a46662416ff7b1d1b8c5cf9280ed128ca3d0
Mathieu David <mathieudavid@mathieudavid.org>
2015-08-13 08:04:27 +0000

    rebuild pages at abae215

--- implementors/mdbook/utils/trait.PathExt.js
+++ implementors/mdbook/utils/trait.PathExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];
+implementors['mdbook'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
