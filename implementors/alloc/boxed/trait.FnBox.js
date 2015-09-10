cfbbedc7c7cdfae9ed2dee2c44b034c321b799d9
Mathieu David <mathieudavid@mathieudavid.org>
2015-09-10 21:32:54 +0000

    rebuild pages at 6962731

--- implementors/alloc/boxed/trait.FnBox.js
+++ implementors/alloc/boxed/trait.FnBox.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['log'] = [];implementors['regex_syntax'] = [];
+implementors['regex_syntax'] = [];implementors['log'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
