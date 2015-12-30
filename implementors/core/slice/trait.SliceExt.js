6b44177c1ec6a1850e7e0983c5839176f5457656
Mathieu David <mathieudavid@mathieudavid.org>
2015-12-30 16:29:39 +0000

    rebuild pages at 9c5c8a6

--- implementors/core/slice/trait.SliceExt.js
+++ implementors/core/slice/trait.SliceExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['handlebars'] = [];
+implementors['libc'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
