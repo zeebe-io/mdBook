e3a21596e011072b9aa8864f5109d2eaff8babb6
Mathieu David <mathieudavid@mathieudavid.org>
2015-12-30 15:02:18 +0000

    rebuild pages at dc8f6cd

---
+++ implementors/book/bookitem/rustc_serialize/json/trait.ToJson.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['mdbook'] = ["impl <a class='trait' href='book/bookitem/rustc_serialize/json/trait.ToJson.html' title='book::bookitem::rustc_serialize::json::ToJson'>ToJson</a> for <a class='struct' href='mdbook/book/bookitem/struct.Chapter.html' title='mdbook::book::bookitem::Chapter'>Chapter</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
