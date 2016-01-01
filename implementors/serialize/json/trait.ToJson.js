98d7ed0befe4c29831edc012f126bafa7a5d8e87
Mathieu David <mathieudavid@mathieudavid.org>
2016-01-01 01:21:04 +0000

    rebuild pages at a7f329d

--- implementors/serialize/json/trait.ToJson.js
+++ implementors/serialize/json/trait.ToJson.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];
+implementors['handlebars'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
