641982f56888a849224d70db5883f82708c9d71c
Mathieu David <mathieudavid@mathieudavid.org>
2016-02-15 20:04:19 +0000

    rebuild pages at 75b98d7

--- clap/args/subcommand/sidebar-items.js
+++ clap/args/subcommand/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["SubCommand","The abstract representation of a command line subcommand used by the consumer of the library."]]});
+initSidebarItems({"struct":[["SubCommand","The abstract representation of a command line subcommand used by the consumer of the library.This struct is used by the library consumer and describes all the valid options of the subcommand for their program. SubCommands are treated like \"sub apps\" and contain all the same possibilities (such as their own arguments and subcommands).Examples"]]});
