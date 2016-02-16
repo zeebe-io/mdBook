7ec2f4709c9278d5208756c48935bf7922ba7710
Mathieu David <mathieudavid@mathieudavid.org>
2016-02-16 09:32:54 +0000

    rebuild pages at fc86b96

--- implementors/core/str/pattern/trait.TwoWayStrategy.js
+++ implementors/core/str/pattern/trait.TwoWayStrategy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bitflags'] = [];implementors['libc'] = [];implementors['regex_syntax'] = [];
+implementors['regex_syntax'] = [];implementors['bitflags'] = [];implementors['libc'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
