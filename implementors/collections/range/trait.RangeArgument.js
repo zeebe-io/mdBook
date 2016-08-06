060d08a798f0cad9ff396fc144f5b7d250b487b5
Mathieu David <mathieudavid@mathieudavid.org>
2016-08-06 22:13:43 +0000

    rebuild pages at f3f9c93

--- implementors/collections/range/trait.RangeArgument.js
+++ implementors/collections/range/trait.RangeArgument.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["bitflags"] = [];implementors["regex_syntax"] = [];implementors["iron"] = [];
+implementors["regex_syntax"] = [];implementors["bitflags"] = [];implementors["bitflags"] = [];implementors["iron"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
