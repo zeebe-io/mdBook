c33492028a4082226a83235c9a90122a1eee8a0b
Mathieu David <mathieudavid@mathieudavid.org>
2017-01-02 19:02:19 +0000

    rebuild pages at 3a71371

--- implementors/core/ops/trait.BitXorAssign.js
+++ implementors/core/ops/trait.BitXorAssign.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["regex_syntax"] = [];implementors["serde"] = [];implementors["nix"] = [];implementors["mio"] = [];implementors["ws"] = [];implementors["hyper"] = [];implementors["handlebars"] = [];implementors["iron"] = [];
+implementors["lazy_static"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["serde"] = [];implementors["nix"] = [];implementors["mio"] = [];implementors["notify"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitXorAssign.html' title='core::ops::BitXorAssign'>BitXorAssign</a> for <a class='struct' href='notify/op/struct.Op.html' title='notify::op::Op'>Op</a>",];implementors["ws"] = [];implementors["hyper"] = [];implementors["handlebars"] = [];implementors["iron"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
