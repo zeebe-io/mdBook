b4805c9b37688498ed2064cc68657d9c789711a4
Michael Bryan <michaelfbryan@gmail.com>
2018-01-13 19:00:41 +0000

    rebuild pages at a1b6ccc

--- env_logger/fmt/sidebar-items.js
+++ env_logger/fmt/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Formatter","A formatter to write logs into."]]});
+initSidebarItems({"enum":[["Color","The set of available English colors for the terminal foreground/background."],["Target","Log target, either `stdout` or `stderr`."],["WriteStyle","Whether or not to print styles to the target."]],"struct":[["Formatter","A formatter to write logs into."],["Style","A set of styles to apply to the terminal output."],["StyledValue","A value that can be printed using the given styles."],["Timestamp","An [RFC3339] formatted timestamp."]]});
--- implementors/error_chain/trait.ChainedError.js
+++ implementors/error_chain/trait.ChainedError.js
@@ -1,6 +1,7 @@
 (function() {var implementors = {};
 implementors["error_chain"] = [];
 implementors["mdbook"] = ["impl <a class=\"trait\" href=\"error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"mdbook/errors/struct.Error.html\" title=\"struct mdbook::errors::Error\">Error</a>",];
+implementors["toml_query"] = ["impl <a class=\"trait\" href=\"error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"toml_query/error/struct.Error.html\" title=\"struct toml_query::error::Error\">Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
